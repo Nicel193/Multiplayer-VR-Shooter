ab1559e22da0372ec47b67a76360ae5d47f6eb38
admin <nicelgoog@gmail.com>
2024-05-10 00:40:34 +0300

    Fix death teleport player

--- Assets/Code/Runtime/Logic/PlayerSpawnPosition.cs
+++ Assets/Code/Runtime/Logic/PlayerSpawnPosition.cs
@@ -4,13 +4,15 @@ namespace Code.Runtime.Logic
 {
     public class PlayerSpawnPosition : MonoBehaviour
     {
+        private const float YPlayerOffset = 0.2f;
+        
         [field: SerializeField] public float Radius { get; private set; }
 
         public Vector3 GetSpawnPosition()
         {
             Vector2 insideUnitCircle = Random.insideUnitCircle * Radius;
             
-            return transform.position + new Vector3(insideUnitCircle.x, 0f, insideUnitCircle.y);
+            return transform.position + new Vector3(insideUnitCircle.x, YPlayerOffset, insideUnitCircle.y);
         }
     }
 }
