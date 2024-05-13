65b7ba9a1e90c91eb69f6ee4aa4736c079bd33e7
admin <nicelgoog@gmail.com>
2024-05-13 14:41:54 +0300

    Fix locomotion move

--- Assets/Plugins/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
+++ Assets/Plugins/Lets Make a VR Game/Oculus Hands/Scripts/AnimateHandOnInput.cs
@@ -1,22 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using Fusion;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
-public class AnimateHandOnInput : MonoBehaviour
+public class AnimateHandOnInput : NetworkBehaviour
 {
-    public InputActionProperty pinchAnimationAction;
-    public InputActionProperty gripAnimationAction;
-    public Animator handAnimator;
-
-    // Update is called once per frame
-    void Update()
+    private const string TriggerAnimationValueTag = "Trigger";
+    private const string GripAnimationValueTag = "Grip";
+    
+    [SerializeField] private InputActionProperty pinchAnimationAction;
+    [SerializeField] private InputActionProperty gripAnimationAction;
+    [SerializeField] private Animator handAnimator;
+    
+    private void Update()
     {
+        if(!Object.HasStateAuthority) return;
+
         float triggerValue = pinchAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Trigger", triggerValue);
+        handAnimator.SetFloat(TriggerAnimationValueTag, triggerValue);
 
         float gripValue = gripAnimationAction.action.ReadValue<float>();
-        handAnimator.SetFloat("Grip", gripValue);
+        handAnimator.SetFloat(GripAnimationValueTag, gripValue);
     }
 }
